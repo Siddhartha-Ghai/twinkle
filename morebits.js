1080ce1c4551030fbb1ece43df4f6fe0bbc39872
Amalthea <amalthea.wikimedia@googlemail.com>
2013-11-08 14:00:43 +0100

    morebits: allow page protections to set watchlist parameter

--- morebits.js
+++ morebits.js
@@ -2151,7 +2151,8 @@ Morebits.wiki.page = function(pageName, currentAction) {
 			prop: 'info',
 			inprop: 'protection',
 			intoken: 'protect',
-			titles: ctx.pageName
+			titles: ctx.pageName,
+			watchlist: ctx.watchlistOption
 		};
 		if (ctx.followRedirect) {
 			query.redirects = '';  // follow all redirects
