00549b6da8ce38df683f49caa1c4c498cc1bcd0e
This, that and the other <at.light@live.com.au>
2013-11-08 21:36:13 +1100

    arv: Add watchlist preference for SPI reporting
    Per request at WT:TW. I haven't tested this, but I'm fairly confident it
    will work.
    
    Conflicts:
    	modules/twinkleconfig.js

--- modules/twinkleconfig.js
+++ modules/twinkleconfig.js
@@ -160,6 +160,18 @@ Twinkle.config.sections = [
 },
 
 {
+	title: "ARV",
+	preferences: [
+		{
+			name: "spiWatchReport",
+			label: "Add sockpuppet report pages to watchlist",
+			type: "enum",
+			enumValues: Twinkle.config.commonEnums.watchlist
+		}
+	]
+},
+
+{
 	title: "Revert and rollback",  // twinklefluff module
 	preferences: [
 		// TwinkleConfig.openTalkPage (array)
